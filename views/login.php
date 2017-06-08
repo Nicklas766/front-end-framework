ff00e299a272e6def62417024daace81daa69fb8
Nicklas766 <Nicklas766@live.se>
2017-06-08 13:07:17 +0200

    login-style

--- views/login.php
+++ views/login.php
@@ -1,20 +1,18 @@
 
 
 
-<div class="content-wrapper">
 
 
 <div class="login-wrapper">
     <div class="login-widget">
-    <h3> Welcome to LogoText </h3>
-    <form action="" method="POST">
-        <input placeholder="Username" type="text" name="name">
+        <h3> Welcome to LogoText </h3>
+        <form action="" method="POST">
+            <input placeholder="Username" type="text" name="name">
 
-        <input placeholder="Password" type="password" name="pass">
+            <input placeholder="Password" type="password" name="pass">
 
-        <input type="submit" name="submitForm" value="Login">
+            <button type="submit" name="submitForm">Login</button>
 
-    </form>
-</div>
+        </form>
     </div>
 </div>
